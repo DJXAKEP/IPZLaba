fe3482debb3f4418fab26851799d17075f6d17f9
Dima <dimattt92@mail.ru>
2013-06-04 22:50:26 +0300

    Client: Repaired JSON query and config file.

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -7,7 +7,6 @@ using System.Net;
 using System.Net.Sockets;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-//using System.Linq;
 
 namespace Client
 {
@@ -18,24 +17,32 @@ namespace Client
 		static void Main( string[] args )
 		{
 			endPoint = parseNetConfig( args );
-
+			Console.WriteLine( "Connected to " + endPoint.Address.ToString()
+					+ ":" + endPoint.Port.ToString() );
 		}
 
 		private static IPEndPoint parseNetConfig( string[] args )
 		{
-			//endPoint = new IPEndPoint();
-			JObject config;
+			JObject config=null;
 			IPAddress ip=null;
 			int port=0;
 
 			if( args.Length < 3 ) {
-				config = (JObject) JToken.ReadFrom( new JsonTextReader( File.OpenText( "config.json" ) ) );
-				if( !int.TryParse( config["netconfig.port"].ToString(), out port ) ) {
+				try {
+					config = (JObject) JToken.ReadFrom( new JsonTextReader( File.OpenText( @"config.json" ) ) );
+				}
+				catch(FileNotFoundException) {
+					Console.Error.WriteLine("error: No \"config.json\" file.");
+					throw new FileNotFoundException();
+				}
+
+				
+				if( !int.TryParse( config["netconfig"]["port"].ToString(), out port ) ) {
 					Console.Error.WriteLine( "error: Wrong port in config file." );
 					throw new NetConfigNotFoundException();
 				}
 				if( args.Length < 2 ) {
-					if( !IPAddress.TryParse( config["netconfig.port"].ToString(), out ip ) ) {
+					if( !IPAddress.TryParse( config["netconfig"]["ip"].ToString(), out ip ) ) {
 						Console.Error.WriteLine( "error: Wrong IP in config file." );
 						throw new NetConfigNotFoundException();
 					}
